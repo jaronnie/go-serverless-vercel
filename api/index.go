aa9f6e73b75a7ad4b3bac7043390355321c745e9
jaron <jaron@jaronnie.com>
2024-09-17 11:42:20 +0800

    feat(go): update

--- api/index.go
+++ api/index.go
@@ -12,7 +12,10 @@ func init() {
 	engine = gin.New()
 	engine.GET("/github/image/pulls", func(context *gin.Context) {
 		response := map[string]any{
-			"message": "120",
+			"schemaVersion": 1,
+			"label":         "pulls",
+			"message":       "120",
+			"color":         "blue",
 		}
 		marshal, _ := json.Marshal(response)
 		_, _ = context.Writer.Write(marshal)
