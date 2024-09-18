ac2364c8e7023a83060bffff1c2b5e0dc70d2d1b
jaron <jaron@jaronnie.com>
2024-09-18 20:59:45 +0800

    Update client.go

--- api/client.go
+++ api/client.go
@@ -1,13 +1,13 @@
 package api
 
 import (
-	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/rest"
 	"go-serverless-vercel/server/config"
 	"go-serverless-vercel/server/handler"
 	"go-serverless-vercel/server/middleware"
 	"go-serverless-vercel/server/svc"
+	"go-serverless-vercel/server/service"
 	"net/http"
 	"os"
 )
