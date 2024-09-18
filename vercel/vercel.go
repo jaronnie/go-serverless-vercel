1642c4c4861fbafccdf20218426275664cfcd191
jaron <jaron@jaronnie.com>
2024-09-18 11:15:32 +0800

    feat(go): update

--- vercel/vercel.go
+++ vercel/vercel.go
@@ -1,8 +1,7 @@
 package vercel
 
 import (
-	"flag"
-	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
 	"github.com/zeromicro/go-zero/rest"
 	"go-serverless-vercel/api/config"
 	"go-serverless-vercel/api/handler"
@@ -11,18 +10,24 @@ import (
 )
 
 var (
-	server     *rest.Server
-	configFile = flag.String("f", "../etc/etc.yaml", "set config file")
+	server *rest.Server
+	//configFile = flag.String("f", "../etc/etc.yaml", "set config file")
 )
 
 func init() {
-	flag.Parse()
-
+	//flag.Parse()
+	//
 	var c config.Config
-	conf.MustLoad(*configFile, &c)
+	//conf.MustLoad(*configFile, &c)
+	c.RestConf = rest.RestConf{
+		Host: "0.0.0.0",
+		Port: 8001, g
+		ServiceConf: service.ServiceConf{
+			Name: "go-serverless-vercel",
+		},
+	}
 
 	server = rest.MustNewServer(c.RestConf)
-	defer server.Stop()
 
 	ctx := svc.NewServiceContext(c)
 	handler.RegisterHandlers(server, ctx)
