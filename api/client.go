2bd36ee4def3b44a4727e64cfd0c19ea25fa1d25
jaron <jaron@jaronnie.com>
2024-09-18 12:02:58 +0800

    feat(go): update

--- api/client.go
+++ api/client.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/service"
 	"github.com/zeromicro/go-zero/rest"
 	"go-serverless-vercel/server/config"
@@ -8,6 +9,7 @@ import (
 	"go-serverless-vercel/server/middleware"
 	"go-serverless-vercel/server/svc"
 	"net/http"
+	"os"
 )
 
 var (
@@ -24,11 +26,24 @@ func init() {
 		},
 	}
 
+	// set up logger
+	if err := logx.SetUp(c.Log.LogConf); err != nil {
+		logx.Must(err)
+	}
+	if c.Log.LogConf.Mode != "console" {
+		logx.AddWriter(logx.NewWriter(os.Stdout))
+	}
+
 	server = rest.MustNewServer(c.Rest.RestConf)
 	middleware.RegisterMiddlewares(server)
 
-	ctx := svc.NewServiceContext(c)
-	handler.RegisterHandlers(server, ctx)
+	svcCtx := svc.NewServiceContext(c)
+	handler.RegisterHandlers(server, svcCtx)
+
+	// server add custom routes
+	svcCtx.Custom.AddRoutes(server)
+
+	logx.Infof("Starting rest server at %s:%d...", svcCtx.Config.Rest.Host, svcCtx.Config.Rest.Port)
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
